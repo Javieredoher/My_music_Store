f85211541f95c5611e797849c3349a18fe6de301
Javieredoher <javieredoher.co@gmail.com>
2021-08-18 22:34:22 -0500

    Update I2

--- docs/script.js
+++ docs/script.js
@@ -21,26 +21,48 @@ close_btn.addEventListener('click', ()=>{
     container.appendChild(square)
 }*/
 
-const iniText = document.getElementsByClassName('iniText')
-const InitImg = document.getElementById('InitImg')
 
-console.log(iniText)
+function api (){
+
+const xhr = new XMLHttpRequest()
+$first =document.getElementById('first'),
+$second =document.getElementById('second'),
+$third =document.getElementById('third'),
+$fourth =document.getElementById('fourth'),
+$fiveth =document.getElementById('fiveth');
+
+xhr.addEventListener('readystatechange', (e)=>{
+    if(xhr.readyState!==4){
+        return
+    } else{
+    console.log(xhr) 
+    }
+
+    if(xhr.status>=200){
+        console.log('Sucess')
+        let json = JSON.parse(xhr.responseText)
+        console.log(json)
+        console.log(json.data)
+
+        const datos = json.data.map(data=>data.images.downsized.url)
+        console.log(datos)
+
+        $first.style.backgroundImage= `url(${datos[0]})`
+        $second.style.backgroundImage= `url(${datos[1]})`
+        $third.style.backgroundImage= `url(${datos[2]})`
+        $fourth.style.backgroundImage= `url(${datos[3]})`
+        $fiveth.style.backgroundImage= `url(${datos[4]})`
+    }
+    else{
+        console.log('error')
+    }
+
+})
 
 
-function displayChange() {
-
-    InitImg.addEventListener('mouseover',()=>{
-        console.log('hola')
-        iniText.style.display= 'flex'
-        // iniText.style.display='flex'
-    })
-    
-    InitImg.addEventListener('mouseout',()=>{
-        InitImg.style.display= 'flex'
-    })
-
+xhr.open("GET","https://api.giphy.com/v1/gifs?api_key=s9OSU4zj9wrxyfw9O5MolrviOKYVujbR&ids=voHUEbyWiHe5a+%2C1rSioV1UX6ZWZv9n4q+%2Cl0HlvGByFLvFJreYE+%2Cl2a8oapqbUhlanPK0m+%2CqVDkVk4xo0q0U")
+xhr.send();
 
 }
 
-
-
+api();
